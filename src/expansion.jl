8cec98863f8a0488521d4502ca0a0a15183cfa51
Felix Gerick <felixgerick@gmail.com>
2019-11-01 15:25:11 +0100

    arbitrary precision gemv

--- src/expansion.jl
+++ src/expansion.jl
@@ -1,6 +1,151 @@
 using Random
 using LinearAlgebra
-using LinearAlgebra.BLAS: gemv!
+# using LinearAlgebra.BLAS: gemv!
+import LinearAlgebra.BLAS.gemv!
+function gemv!(trans::AbstractChar, alpha::Union{(T), Bool},
+               A::AbstractVecOrMat{T}, X::AbstractVector{T},
+               beta::Union{(T), Bool}, Y::AbstractVector{T}) where T <: Number
+    # Base.require_one_based_indexing(A, X, Y)
+    m,n = size(A,1),size(A,2)
+    if trans == 'N' && (length(X) != n || length(Y) != m)
+        throw(DimensionMismatch("A has dimensions $(size(A)), X has length $(length(X)) and Y has length $(length(Y))"))
+    elseif trans == 'C' && (length(X) != m || length(Y) != n)
+        throw(DimensionMismatch("the adjoint of A has dimensions $n, $m, X has length $(length(X)) and Y has length $(length(Y))"))
+    elseif trans == 'T' && (length(X) != m || length(Y) != n)
+        throw(DimensionMismatch("the transpose of A has dimensions $n, $m, X has length $(length(X)) and Y has length $(length(Y))"))
+    end
+    LinearAlgebra.chkstride1(A)
+     _gemv!(trans, size(A,1), size(A,2), alpha,
+             A, max(1,stride(A,2)), X, stride(X,1),
+             beta, Y, stride(Y,1))
+    Y
+end
+
+Base.@inline function _gemv!(TRANS::Char, M::Int, N::Int, ALPHA::T, A::AbstractVecOrMat{T},
+                LDA::Int, X::AbstractVector{T}, INCX::Int, BETA::T,
+                Y::AbstractVector{T}, INCY::Int) where {T <: Number}
+
+    INFO = 0
+    if (TRANS != 'N') && (TRANS != 'T') && (TRANS != 'C')
+        INFO = 1
+    elseif (M<0)
+        INFO = 2
+    elseif (N<0)
+        INFO = 3
+    elseif (LDA < max(1,M))
+        INFO = 6
+    elseif (INCX==0)
+        INFO = 8
+    elseif (INCY==0)
+        INFO = 11
+    end
+    @assert INFO==0
+
+    if ((M == 0) || (N == 0) || ((ALPHA==zero(T)) && (BETA == one(T))))
+        return
+    end
+
+
+    if (TRANS=='N')
+        LENX = N
+        LENY = M
+    else
+        LENX = M
+        LENY = N
+    end
+    if (INCX > 0)
+        KX = 1
+    else
+        KX = 1 - (LENX-1)*INCX
+    end
+    if (INCY > 0)
+        KY = 1
+    else
+        KY = 1 - (LENY-1)*INCY
+    end
+
+    @inbounds begin
+    if (BETA!=one(T))
+        if (INCY==1)
+            if (BETA==zero(T))
+                for I = 1:LENY
+                    Y[I] = zero(T)
+                end
+            else
+                for I = 1:LENY
+                    Y[I] = BETA*Y[I]
+                end
+            end
+        else
+            IY = KY
+            if (BETA==zero(T))
+                for I = 1:LENY
+                    Y[IY] = zero(T)
+                    IY = IY + INCY
+                end
+            else
+                for I = 1:LENY
+                    Y[IY] = BETA*Y[IY]
+                    IY = IY + INCY
+                end
+            end
+        end
+    end
+    if (ALPHA==zero(T))
+        return
+    end
+    if (TRANS == 'N')
+
+        JX = KX
+        if (INCY==1)
+            for J = 1:N
+                TEMP = ALPHA*X[JX]
+                for I = 1:M
+                    Y[I] = Y[I] + TEMP*A[I,J]
+                end
+                JX = JX + INCX
+            end
+        else
+            for J = 1:N
+                TEMP = ALPHA*X[JX]
+                IY = KY
+                for I = 1:M
+                    Y[IY] = Y[IY] + TEMP*A[I,J]
+                    IY = IY + INCY
+                end
+                JX = JX + INCX
+            end
+        end
+    else
+
+        JY = KY
+        if (INCX==1)
+            for J = 1:N
+                TEMP = zero(T)
+               for I = 1:M
+                    TEMP = TEMP + A[I,J]*X[I]
+                end
+                Y[JY] = Y[JY] + ALPHA*TEMP
+                JY = JY + INCY
+            end
+        else
+            for J = 1:N
+                TEMP = zero(T)
+                IX = KX
+                for I = 1:M
+                    TEMP = TEMP + A[I,J]*X[IX]
+                    IX = IX + INCX
+                end
+                Y[JY] = Y[JY] + ALPHA*TEMP
+                JY = JY + INCY
+            end
+        end
+    end
+    end
+    return
+end
+
+
 
 """
     reinitialize!(a::Arnoldi, j::Int = 0) → a
@@ -112,7 +257,7 @@ Perform Arnoldi from `from` to `to`.
 """
 function iterate_arnoldi!(A, arnoldi::Arnoldi{T}, range::UnitRange{Int}) where {T}
     V, H = arnoldi.V, arnoldi.H
-    
+
     for j = range
         # Generate a new column of the Krylov subspace
         mul!(view(V, :, j+1), A, view(V, :,j))
